0654b458a5f67fbf572ed457d298af7a0c492f48
Jeffrey-Ragland <jeffreyragland3009@gmail.com>
2024-05-17 00:39:25 +0530

    home page, hamburger 75% completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 import xyma from '../Assets/xymalogo_white.png';
 import { HeaderData } from "../data/HeaderData";
 import CircleComponent from "../CircleComponent";
-import { IoMenu } from "react-icons/io5";
-
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
+import { FiArrowUpRight } from "react-icons/fi";
 
 const Navbar = () => {
   //const navigate = useNavigate();
@@ -16,6 +16,7 @@ const Navbar = () => {
   // };
   const navigate = useNavigate();
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [burgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
   const handleNavItemClick = (index) => {
     setSelectedIndex(index);
@@ -25,6 +26,11 @@ const Navbar = () => {
     navigate("/contact");
     setSelectedIndex(null);
   };
+
+  const handleBurgerMenu = () =>
+  {
+    setBurgerMenuOpen(!burgerMenuOpen);
+  }
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50">
@@ -59,9 +65,22 @@ const Navbar = () => {
           >
             Get in Touch
           </div>
-          <div className="flex items-center justify-center md:hidden">
-            <IoMenu size={30} />
-          </div>
+          {!burgerMenuOpen && (
+            <div
+              className="flex items-center justify-center md:hidden cursor-pointer"
+              onClick={handleBurgerMenu}
+            >
+              <IoMenu size={30} />
+            </div>
+          )}
+          {burgerMenuOpen && (
+            <div
+              className="flex items-center justify-center md:hidden cursor-pointer"
+              onClick={handleBurgerMenu}
+            >
+              <IoCloseSharp size={30} />
+            </div>
+          )}
         </div>
         {/* <button
             className='text-white py-3 px-4 rounded-full'
@@ -101,6 +120,39 @@ const Navbar = () => {
           background: "linear-gradient(90deg, #FE6F17 0%, #FE9D1C 101.48%)",
         }}
       ></div>
+      {burgerMenuOpen && (
+        <div className="bg-white h-[90vh] w-full sm:hidden">
+          <div className="h-[60%]">
+            {HeaderData.map((item, index) => (
+              <div
+                key={index}
+                className={`w-full h-1/6 border border-b-gray-300 flex items-center font-[700] text-lg  ${
+                  selectedIndex === index ? "text-orange-400" : ""
+                }`}
+                onClick={() => {
+                  handleNavItemClick(index);
+                  handleBurgerMenu();
+                }}
+              >
+                <NavLink
+                  to={item.path}
+                  className="w-full h-full flex items-center justify-between px-4"
+                >
+                  {/* <div className="border border-black w-full h-full"> */}
+                  {item.title}
+                  <div
+                    className={`${selectedIndex === index ? "text-orange-400" : "text-gray-400"}`}
+                  >
+                    <FiArrowUpRight size={25} />
+                  </div>
+                  {/* </div> */}
+                </NavLink>
+                {/* <div></div> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
